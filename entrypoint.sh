c6429857c7cd26d80b065a762c8ae808b5a42a66
sharif <61516383+sharifhsn@users.noreply.github.com>
2022-02-26 12:21:11 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,6 +72,7 @@ main() {
         echo "Pushing artifacts to ${TARGET_REPOSITORY}:$remote_branch"
 
         cd public
+        touch .nojekyll
         git init
         git config user.name "GitHub Actions"
         git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
